9fecef2bed763868094e9b813ff8ff7872874929
Artem Medvediev <medved196922@gmail.com>
2022-02-14 13:33:27 +0200

    implemented user profile data model

---
+++ lib/data/user_profile.dart
@@ -0,0 +1,21 @@
+class UserProfile {
+  final String name;
+  final String email;
+
+  UserProfile({
+    required this.name,
+    required this.email,
+  });
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) {
+    return UserProfile(
+      name: json['name'],
+      email: json['email'],
+    );
+  }
+
+  toJson() => {
+        'name': name,
+        'email': email,
+      };
+}
